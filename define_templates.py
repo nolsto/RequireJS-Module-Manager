178015e9be8be1ab107d2ee0cf31daea7afb1d95
Nolan <nolan@nolsto.com>
2013-08-02 19:45:26 -0700

    Added AMD define js templates.

---
+++ define_templates.py
@@ -0,0 +1,41 @@
+# AMD split resources and arguments on lines
+template_amd_stacked = """
+define([
+    'module_id',
+    'path/to/other_module',
+    'another_module_id'
+], function(
+    module,
+    otherModule
+) {
+    {{script}}
+});
+"""
+
+# AMD split array and function on lines
+template_amd_split = """
+define(
+    ['module_id', 'path/to/other_module', 'another_module_id'],
+    function(module, otherModule) {
+        {{script}}
+    }
+);
+"""
+
+# AMD single-line define
+template_amd_single_line = """
+define(['module_id', 'path/to/other_module', 'another_module_id'], function(module, otherModule) {
+    {{script}}
+});
+"""
+
+# CommonJS
+template_commonjs = """
+define(function (require) {
+    var module = require('module_id');
+    var otherModule = require('path/to/other_module');
+    require('another_module_id');
+
+    {{script}}
+});
+"""
