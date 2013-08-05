e33fc929873bb49787fe524874e1222144b64547
Nolan <nolan@nolsto.com>
2013-08-05 01:07:45 -0700

    Simplified define templates. Added amd define snippet and renamed the commonjs one. Attempting to parse a string of js source containing a define.

--- template.py
+++ template.py
@@ -1,8 +1,52 @@
 import re
 
 
-define_regex = re.compile(r"""
+# define_regex = re.compile(r"""
+#     ^define\s*\(\s*               # define invocation
+#     \[\s*                         # module arrary opening bracket
+#     (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
+#     .+                            # the module item
+#     (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
+#     \s*\],\s*                     # closing bracket
+#     function\s*\(\s*              # function definition
+#     (?P<args>(                    # function args opening paren
+#     [a-zA-Z_$][0-9a-zA-Z_$]*      # the module argument name
+#     (?:,\s*)?)+)                  # function args closing paren
+#     \s*\)\s*{\s*                  # function block begin
+#     (?P<script>.+)?               # everything in-between
+#     \s*}\s*\)                     # end function definition
+# """, re.VERBOSE)
+
+comment_regex = re.compile(r"""
+    (^)?                # beginning of line
+    [^\S\n]*            # match any whitespace character except newline
+    /(?:\*(.*?)\*/      # captures multiline comment content
+    [^\S\n]*            # match any whitespace character except newline
+    |
+    /[^\n]*             # matches single-line comment
+    )                   # end comment content capture
+    ($)?                # end of line
+""", re.DOTALL | re.MULTILINE | re.VERBOSE)
+
+define_template_regex = re.compile(r"""
     ^define\s*\(\s*               # define invocation
+    \[\s*                         # module arrary opening bracket
+    (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
+    {{module}}                    # the module item
+    (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
+    \s*\],\s*                     # closing bracket
+    function\s*\(\s*              # function definition
+    (?P<args>(                    # function args opening paren
+    {{name}}                      # the module argument name
+    (?:,\s*)?)+)                  # function args closing paren
+    \s*\)\s*{\s*                  # function block begin
+    {{script}}                    # everything in-between
+    \s*}\s*\)                     # end function definition
+""", re.VERBOSE)
+
+parse_define_regex = re.compile(r"""
+    ^[^\S\n]*                     # beginning of line + whitespace
+    define\s*\(\s*                # define invocation
     \[\s*                         # module arrary opening bracket
     (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
     .+                            # the module item
@@ -15,7 +59,7 @@ define_regex = re.compile(r"""
     \s*\)\s*{\s*                  # function block begin
     (?P<script>.+)?               # everything in-between
     \s*}\s*\)                     # end function definition
-""", re.VERBOSE)
+""", re.MULTILINE | re.VERBOSE)
 
 # define_mods_regex = re.compile(r"""
 
@@ -37,10 +81,15 @@ class Template:
         return self.string
 
     def validate(self):
-        match = define_regex.search(self.string)
+        match = define_template_regex.search(self.string)
         # print '<Match: %r, groups=%r>' % (match.group(), match.groups())
         # print match.group('quote')
-        print match.group('mods')
-        print match.group('args')
+        # print match.group('mods')
+        # print match.group('args')
         # print match.groups()
         return match is not None
+
+    def parse(self, string_input):
+        match = parse_define_regex.search(string_input)
+        return match
+
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,6 +1,7 @@
 import unittest
 
 from template import Template
+from template import parse_define_regex
 
 
 class TemplatesTest(unittest.TestCase):
@@ -11,37 +12,44 @@ class TemplatesTest(unittest.TestCase):
     def test_flattens_strings_list(self):
         string_list = [
             "define([",
-            "    '{{module_a}}',",
-            "    '{{module_b}}',",
-            "    '{{module_c}}'",
+            "    '{{module}}'",
             "], function(",
-            "    {{name_a}},",
-            "    {{name_b}}",
+            "    {{name}}",
             ") {",
             "    {{script}}",
             "});"
         ]
         template = Template(string_list)
-        template_string = "define([\n    '{{module_a}}',\n    '{{module_b}}',\n    '{{module_c}}'\n], function(\n    {{name_a}},\n    {{name_b}}\n) {\n    {{script}}\n});"
+        template_string = "define([\n    '{{module}}'\n], function(\n    {{name}}\n) {\n    {{script}}\n});"
 
         self.assertEqual(template_string, template.string)
 
-    def test_single_line_template_passes_regex(self):
+    # def test_single_line_template_is_valid(self):
+    #     string_list = [
+    #         r"define(['module_a', '/path/to/mo\'dule_b', '!text/module_c.hbs'], function(name_a, name_b) {",
+    #         "    script",
+    #         "});"
+    #     ]
+    #     template = Template(string_list)
+
+    #     self.assertTrue(template.validate())
+
+    def test_single_line_template_is_valid(self):
         string_list = [
-            r"define(['module_a', '/path/to/mo\'dule_b', '!text/module_c.hbs'], function(name_a, name_b) {",
-            "    script",
+            "define(['{{module}}'], function({{name}}) {",
+            "    {{script}}",
             "});"
         ]
         template = Template(string_list)
 
         self.assertTrue(template.validate())
 
-    def test_split_args_template_passes_regex(self):
+    def test_split_args_template_is_valid(self):
         string_list = [
             "define(",
-            "    ['module_a', '/path/to/module_b', '!text/module_c.hbs'],",
-            "    function(name_a, name_b) {",
-            "        script",
+            "    ['{{module}}'],",
+            "    function({{name}}) {",
+            "        {{script}}",
             "    }",
             ");"
         ]
@@ -49,22 +57,41 @@ class TemplatesTest(unittest.TestCase):
 
         self.assertTrue(template.validate())
 
-    def test_stacked_template_passes_regex(self):
+    def test_stacked_template_is_valid(self):
         string_list = [
             "define([",
-            "    'module_a',",
-            "    '/path/to/module_b',",
-            "    '!text/module_c.hbs'",
+            "    '{{module}}'",
             "], function(",
-            "    name_a,",
-            "    name_b",
+            "    {{name}}",
             ") {",
-            "    script",
+            "    {{script}}",
             "});"
         ]
         template = Template(string_list)
 
         self.assertTrue(template.validate())
+
+    # def test_commonjs_template_is_valid(self):
+    #     string_list = [
+    #         "define(function (require) {",
+    #         "    var {{name}} = require('{{module}}');",
+    #         "",
+    #         "    {{script}}",
+    #         "});"
+    #     ]
+    #     template = Template(string_list)
+
+    #     self.assertTrue(template.validate())
+
+    def test_parses_string_input(self):
+        string_buffer = """define ([ "module_a", "path/to/module_b", "./module_c", "!text/module_d.hbs" ],
+                    function (moduleA, _moduleB, $moduleC, moduleD) {
+
+                    });
+        """
+        # print string_buffer
+        # parse_define_regex.match(string_buffer)
+        self.assertIsNotNone(parse_define_regex.match(string_buffer))
 
 
 if __name__ == '__main__':
