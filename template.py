298bc090d2bfaab930ef3e51a4ad0cc2a8dc42d1
Nolan <nolan@nolsto.com>
2013-08-11 19:18:23 -0700

    Began rewriting template tests for snippet sources.

--- template.py
+++ template.py
@@ -17,79 +17,91 @@ import re
 #     \s*}\s*\)                     # end function definition
 # """, re.VERBOSE)
 
-comment_regex = re.compile(r"""
-    (^)?                # beginning of line
-    [^\S\n]*            # match any whitespace character except newline
-    /(?:\*(.*?)\*/      # captures multiline comment content
-    [^\S\n]*            # match any whitespace character except newline
-    |
-    /[^\n]*             # matches single-line comment
-    )                   # end comment content capture
-    ($)?                # end of line
-""", re.DOTALL | re.MULTILINE | re.VERBOSE)
+# comment_regex = re.compile(r"""
+#     (^)?                # beginning of line
+#     [^\S\n]*            # match any whitespace character except newline
+#     /(?:\*(.*?)\*/      # captures multiline comment content
+#     [^\S\n]*            # match any whitespace character except newline
+#     |
+#     /[^\n]*             # matches single-line comment
+#     )                   # end comment content capture
+#     ($)?                # end of line
+# """, re.DOTALL | re.MULTILINE | re.VERBOSE)
 
-define_template_regex = re.compile(r"""
-    ^define\s*\(\s*               # define invocation
-    \[\s*                         # module arrary opening bracket
-    (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
-    {{module}}                    # the module item
-    (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
-    \s*\],\s*                     # closing bracket
-    function\s*\(\s*              # function definition
-    (?P<args>(                    # function args opening paren
-    {{name}}                      # the module argument name
-    (?:,\s*)?)+)                  # function args closing paren
-    \s*\)\s*{\s*                  # function block begin
-    {{script}}                    # everything in-between
-    \s*}\s*\)                     # end function definition
-""", re.VERBOSE)
+# define_template_regex = re.compile(r"""
+#     ^define\s*\(\s*               # define invocation
+#     \[\s*                         # module arrary opening bracket
+#     (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
+#     {{module}}                    # the module item
+#     (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
+#     \s*\],\s*                     # closing bracket
+#     function\s*\(\s*              # function definition
+#     (?P<args>(                    # function args opening paren
+#     {{name}}                      # the module argument name
+#     (?:,\s*)?)+)                  # function args closing paren
+#     \s*\)\s*{\s*                  # function block begin
+#     {{script}}                    # everything in-between
+#     \s*}\s*\)                     # end function definition
+# """, re.VERBOSE)
 
-parse_define_regex = re.compile(r"""
-    ^[^\S\n]*                     # beginning of line + whitespace
-    define\s*\(\s*                # define invocation
-    \[\s*                         # module arrary opening bracket
-    (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
-    .+                            # the module item
-    (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
-    \s*\],\s*                     # closing bracket
-    function\s*\(\s*              # function definition
-    (?P<args>(                    # function args opening paren
-    [a-zA-Z_$][0-9a-zA-Z_$]*      # the module argument name
-    (?:,\s*)?)+)                  # function args closing paren
-    \s*\)\s*{\s*                  # function block begin
-    (?P<script>.+)?               # everything in-between
-    \s*}\s*\)                     # end function definition
-""", re.MULTILINE | re.VERBOSE)
+# parse_define_regex = re.compile(r"""
+#     ^[^\S\n]*                     # beginning of line + whitespace
+#     define\s*\(\s*                # define invocation
+#     \[\s*                         # module arrary opening bracket
+#     (?P<mods>((?P<quote>['"])     # the captured opening quote: ' or "
+#     .+                            # the module item
+#     (?<!\\)(?P=quote)(?:,\s*)?)+) # closing quote from above that is not preceded by backslash
+#     \s*\],\s*                     # closing bracket
+#     function\s*\(\s*              # function definition
+#     (?P<args>(                    # function args opening paren
+#     [a-zA-Z_$][0-9a-zA-Z_$]*      # the module argument name
+#     (?:,\s*)?)+)                  # function args closing paren
+#     \s*\)\s*{\s*                  # function block begin
+#     (?P<script>.+)?               # everything in-between
+#     \s*}\s*\)                     # end function definition
+# """, re.MULTILINE | re.VERBOSE)
 
 # define_mods_regex = re.compile(r"""
 
 # """, re.VERBOSE|re.MULTILINE)
 
 
-def flatten(string_list):
-    return '\n'.join(string_list)
+snippet_cdata_regex = re.compile(r"""
+    <content>\s*
+    (?:<!\[CDATA\[)?
+    (?P<content>.*?)
+    (?:\]\]>)?\s*
+    <\/content>
+""", re.VERBOSE | re.DOTALL)
+
+
+def get_content_from_snippet(snippet):
+    match = snippet_cdata_regex.search(snippet)
+    return match.group('content')
 
 
 class Template:
     """docstring for Template"""
 
-    def __init__(self, string_list):
-        self.string = flatten(string_list)
+    def __init__(self, string):
+        self.string = string
 
     @property
     def string(self):
         return self.string
 
     def validate(self):
-        match = define_template_regex.search(self.string)
+        # match = define_template_regex.search(self.string)
         # print '<Match: %r, groups=%r>' % (match.group(), match.groups())
         # print match.group('quote')
         # print match.group('mods')
         # print match.group('args')
         # print match.groups()
-        return match is not None
+        # return match is not None
+        pass
 
     def parse(self, string_input):
-        match = parse_define_regex.search(string_input)
-        return match
+        # match = parse_define_regex.search(string_input)
+        # return match
+        pass
 
--- tests/test_templates.py
+++ tests/test_templates.py
@@ -1,7 +1,6 @@
 import unittest
 
-from template import Template
-from template import parse_define_regex
+from template import get_content_from_snippet, Template
 
 
 class TemplatesTest(unittest.TestCase):
@@ -9,20 +8,25 @@ class TemplatesTest(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_flattens_strings_list(self):
-        string_list = [
-            "define([",
-            "    '{{module}}'",
-            "], function(",
-            "    {{name}}",
-            ") {",
-            "    {{script}}",
-            "});"
-        ]
-        template = Template(string_list)
-        template_string = "define([\n    '{{module}}'\n], function(\n    {{name}}\n) {\n    {{script}}\n});"
+    def test_finds_cdata(self):
+        snippet = """<snippet>
+    <content><![CDATA[
+define(${1:}['${2:$TM_MODULE_PATH_PLACEHOLDER}'], function(${3:$TM_MODULE_NAME_PLACEHOLDER}) {
+    ${0:$TM_SELECTED_TEXT}
+});
+]]></content>
+    <tabTrigger>define</tabTrigger>
+    <scope>source.js</scope>
+    <description>define (AMD single line)</description>
+</snippet>"""
 
-        self.assertEqual(template_string, template.string)
+        snippet_content = """
+define(${1:}['${2:$TM_MODULE_PATH_PLACEHOLDER}'], function(${3:$TM_MODULE_NAME_PLACEHOLDER}) {
+    ${0:$TM_SELECTED_TEXT}
+});
+"""
+
+        self.assertEqual(get_content_from_snippet(snippet), snippet_content)
 
     # def test_single_line_template_is_valid(self):
     #     string_list = [
@@ -35,41 +39,43 @@ class TemplatesTest(unittest.TestCase):
     #     self.assertTrue(template.validate())
 
     def test_single_line_template_is_valid(self):
-        string_list = [
-            "define(['{{module}}'], function({{name}}) {",
-            "    {{script}}",
-            "});"
-        ]
-        template = Template(string_list)
+        string = """
+define(${1:}['${2:$TM_MODULE_PATH_PLACEHOLDER}'], function(${3:$TM_MODULE_NAME_PLACEHOLDER}) {
+    ${0:$TM_SELECTED_TEXT}
+});
+"""
+        template = Template(string)
 
         self.assertTrue(template.validate())
 
     def test_split_args_template_is_valid(self):
-        string_list = [
-            "define(",
-            "    ['{{module}}'],",
-            "    function({{name}}) {",
-            "        {{script}}",
-            "    }",
-            ");"
-        ]
-        template = Template(string_list)
+        # string_list = [
+        #     "define(",
+        #     "    ['{{module}}'],",
+        #     "    function({{name}}) {",
+        #     "        {{script}}",
+        #     "    }",
+        #     ");"
+        # ]
+        # template = Template(string_list)
 
-        self.assertTrue(template.validate())
+        # self.assertTrue(template.validate())
+        pass
 
     def test_stacked_template_is_valid(self):
-        string_list = [
-            "define([",
-            "    '{{module}}'",
-            "], function(",
-            "    {{name}}",
-            ") {",
-            "    {{script}}",
-            "});"
-        ]
-        template = Template(string_list)
+        # string_list = [
+        #     "define([",
+        #     "    '{{module}}'",
+        #     "], function(",
+        #     "    {{name}}",
+        #     ") {",
+        #     "    {{script}}",
+        #     "});"
+        # ]
+        # template = Template(string_list)
 
-        self.assertTrue(template.validate())
+        # self.assertTrue(template.validate())
+        pass
 
     # def test_commonjs_template_is_valid(self):
     #     string_list = [
@@ -83,15 +89,15 @@ class TemplatesTest(unittest.TestCase):
 
     #     self.assertTrue(template.validate())
 
-    def test_parses_string_input(self):
-        string_buffer = """define ([ "module_a", "path/to/module_b", "./module_c", "!text/module_d.hbs" ],
-                    function (moduleA, _moduleB, $moduleC, moduleD) {
+    # def test_parses_string_input(self):
+    #     string_buffer = """define ([ "module_a", "path/to/module_b", "./module_c", "!text/module_d.hbs" ],
+    #                 function (moduleA, _moduleB, $moduleC, moduleD) {
 
-                    });
-        """
-        # print string_buffer
-        # parse_define_regex.match(string_buffer)
-        self.assertIsNotNone(parse_define_regex.match(string_buffer))
+    #                 });
+    #     """
+    #     # print string_buffer
+    #     # parse_define_regex.match(string_buffer)
+    #     self.assertIsNotNone(parse_define_regex.match(string_buffer))
 
 
 if __name__ == '__main__':
