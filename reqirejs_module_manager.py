e2dc7bd890a4ed51402977dd5eabe59b1cac8ce4
Nolan Stover <nstover@ag.com>
2013-08-21 16:36:37 -0700

    About to swap bracket regex captures for ST's scope socre_selector.

--- reqirejs_module_manager.py
+++ reqirejs_module_manager.py
@@ -1,17 +1,3 @@
-###
-# Commands
-###
-# Add Dependency
-#   fuzzy select resource or choose to input
-#   if resource selected, input variable name (leave blank for none)
-#   if input selected, input resource, then input variable name as above
-# Modify Dependency
-#   fuzzy select resource
-#   input new resource value (or leave as-is)
-#   input new variable name (or leave as-is)
-# Remove Dependency
-#   fuzzy select path or variable name
-
 import os
 import re
 from itertools import chain
@@ -30,14 +16,28 @@ list_regex = re.compile(r"""
     (?P<array>^\[.*\]$)             # captured is an array in square brackets
     |                               # or,
     (?P<args>^\(.*\)$)              # captured is argument(s) in parenthesis
-""", re.VERBOSE | re.DOTALL)
+""", re.VERBOSE + re.DOTALL)
 
 # matches all strings enclosed in quotation marks
-string_regex = re.compile(r"""
+string_pattern = r"""
     (?P<quote>['"])                 # opening quote (single or double)
     .*?                             # lazy repeat--allows multiple captures
     (?<!\\)(?P=quote)               # unescaped closing quote (same as above)
-""", re.VERBOSE)
+"""
+string_regex = re.compile(string_pattern, re.VERBOSE)
+
+js_comment_regex = re.compile(r"""
+    (/\*(?:[^*]|\*[^/])*\*/)        # multi-line comment
+    |                               # or,
+    (?://(.*)$)                     # single-line comment
+""", re.VERBOSE + re.MULTILINE)
+
+# matches a define function call with optional module name argument
+# if it is at the end of the string.
+# should be run after stripping js comments and whitespace
+define_regex = re.compile(r'define\((%s,)?$' % (string_pattern), re.VERBOSE)
+
+function_regex = re.compile(r'^\),function\(')
 
 
 class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
@@ -85,55 +85,109 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
     def capture(self):
         # get cursor position or span of selection
         original_region = self.view.sel()[0]
-        # get the contents of the selection
-        selection = self.view.substr(original_region)
+        region = original_region
 
         while True:
-            match = list_regex.search(selection)
+            # search the contents of the selected region
+            match = list_regex.search(self.view.substr(region))
             if match:
                 # the selection is either an array or an arguments list
-                # pass it on and then stop looping
+                # pass it to extract method and then stop looping
                 self.extract(**match.groupdict())
                 break
 
             # expand the selection's scope
             self.view.run_command('expand_selection', {'to': 'brackets'})
-            # get the contents of the new selection
-            new_selection = self.view.substr(self.view.sel()[0])
+            # get the new selected region
+            new_region = self.view.sel()[0]
 
-            if new_selection == selection:
+            if new_region == region:
                 # if the new selection is the same as the old one,
-                # we've selected the whole text buffer and need to stop looping
+                # there are no outer brackets and we need to stop looping
                 break
 
-            # set the selection to the selection with increased scope and loop
-            selection = new_selection
+            # set the region to the new region with increased scope and loop
+            region = new_region
+
+        # clear regions and add back the original
+        # TODO: the cursor is still visually in the wrong place though
+        # self.view.sel().clear()
+        # self.view.sel().add(original_region)
+        # self.view.show(original_region)
 
 
     def extract(self, array, args):
-        print array, args
-
         if array:
-            # TODO:
-            # Look for `define(` to left of the array (should allow for an
-            #   optional module name argument).
-            # Look for `), function(...)` to right of the array
-            # Parse strings in array and generate list
-            # Split arguments on commas and generate list
-            # Create dictionary of key-value pairs with arguments and array items
-            # Quick response panel with added items above
-
-            self.window.show_quick_panel(self.items,
-                                         self.handle_path_panel_response,
-                                         sublime.MONOSPACE_FONT, 0)
+            self.extract_from_array(array)
         elif args:
-            # TODO:
-            # Look for `require` to left of the args
-            # parse arguments and exit if the returned is not a single string
-            # look for `var = ` to left of `require`
-            # Create key-value pair with variable name and argument string
-            # show quick response panel with added items above
-            pass
+            self.extract_from_args(args)
+
+
+    def extract_from_array(self, array):
+        # TODO:
+        # Look for `define(` to left of the array (should allow for an
+        #   optional module name argument).
+        # Look for `), function(...)` to right of the array
+        # Parse strings in array and generate list
+        # Split arguments on commas and generate list
+        # Create dictionary of key-value pairs with arguments and array items
+        # Quick response panel with added items above
+
+        buffer_string = self.view.substr(sublime.Region(0, self.view.size()))
+
+        original_region = self.view.sel()[0]
+        region = original_region
+        point = region.begin() - 1
+
+        while True:
+            # get point at beginning of region
+            # move backwards one character
+            # if it is whitespace, loop
+            # if it is a comment, expand region to scope and loop
+            # if it is a comma, loop and look for a string
+
+            char = self.view.substr(point)
+
+            if char == u'\x00':
+                # char is null unicode character--we're at the beginning of
+                # the buffer and need to stop looping
+                break
+
+            print self.view.scope_name(point)
+
+            # self.view.run_command('move', {'by': 'characters', 'forward': False})
+            # new_region = self.view.sel()[0]
+
+            # print self.view.scope_name(point)
+            # print self.view.score_selector(self.view.sel()[0].begin(), 'comment')
+            # print self.view.score_selector(self.view.sel()[0].begin(), 'punctuation')
+
+            point -= 1
+
+        # expand the selection's scope
+        # self.view.run_command('expand_selection', {'to': 'brackets'})
+        # while True:
+        #     match = re.match('define')
+
+        # left_region = sublime.Region(0, region.begin())
+        # js = js_comment_regex.sub('', self.view.substr(left_region))
+        # js = re.sub(r'\s', '', js)
+        # print self.view.substr(right_region)
+        # if define_regex.search(js):
+
+        # self.window.show_quick_panel(self.items,
+        #                              self.handle_path_panel_response,
+        #                              sublime.MONOSPACE_FONT, 0)
+
+
+    def extract_from_args(self, args):
+        # TODO:
+        # Look for `require` to left of the args
+        # parse arguments and exit if the returned is not a single string
+        # look for `var = ` to left of `require`
+        # Create key-value pair with variable name and argument string
+        # show quick response panel with added items above
+        pass
 
 
     def handle_path_panel_response(self, index):
@@ -196,3 +250,4 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
     #     # self.view.run_command('insert_snippet',
     #     #                       {'name': self.define_snippet})
     #     self.view.end_edit(edit)
+
