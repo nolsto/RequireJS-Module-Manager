52b61f28ee0c39ea66bf8f2cd51054774017ae59
Nolan <nolan@nolsto.com>
2013-08-20 22:36:35 -0700

    Added string capture in regex.

--- reqirejs_module_manager.py
+++ reqirejs_module_manager.py
@@ -12,9 +12,11 @@
 # Remove Dependency
 #   fuzzy select path or variable name
 
-import sublime, sublime_plugin
 import os
+import re
 from itertools import chain
+
+import sublime, sublime_plugin
 
 import util
 from module_collection import ModuleCollection
@@ -23,6 +25,23 @@ from template import Template
 
 settings_filename = 'RequireJS Module Manager.sublime-settings'
 
+# matches a string enclosed by either quotation marks,
+# square brackets or parenthesis
+regex = re.compile(r"""
+    (?P<string>^                    # captured is a string in quotations
+        (?P<quote>['"])             # opening quote (single or double)
+        .*
+        (?<!\\)(?P=quote)           # unescaped closing quote (same as above)
+    $)|
+    (?P<array>^                     # captured is array in square brackets
+        \[.*\]
+    $)|
+    (?P<args>^                      # captured is arguments in parenthesis
+        \(.*\)
+    $)
+""", re.VERBOSE)
+
+
 class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
 
     def run(self):
@@ -30,13 +49,6 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
 
         # Active view (area that contains the text buffer)
         self.view = self.window.active_view()
-
-        # print self.view.sel()[0]
-        # print self.view.lines(self.view.sel()[0])
-        # print self.view.find_by_selector('source.js')
-        # print self.view.get_regions('import')
-        # print self.view.extract_scope()
-        self.view.run_command('expand_selection', {'to': 'brackets'})
 
         # First folder in the project
         self.folder = self.window.folders()[0]
@@ -56,11 +68,11 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
         self.define_snippet_path = os.path.join(self.packages_path, '..',
                                                 self.define_snippet)
 
-        # self.template = Template(self.define_snippet)
+        self.mappings = {}
+        self.items = ['> Input path to module...'] + self.module_collection.ids
+        self.names = []
 
-        items = ['> Input path to module...'] + self.module_collection.ids
-        self.window.show_quick_panel(items, self.handle_quick_panel_response,
-                                     sublime.MONOSPACE_FONT, 0)
+        self.capture()
 
 
     def get_setting(self, prop):
@@ -72,7 +84,57 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
         return self.view.settings().get(prop, self.settings.get(prop))
 
 
-    def handle_quick_panel_response(self, index):
+    def capture(self):
+        # get cursor position or span of selection
+        original_region = self.view.sel()[0]
+        # get the contents of the selection
+        selection = self.view.substr(original_region)
+
+        while True:
+            match = regex.search(selection)
+            if match:
+                print match.groupdict()
+                # print match.groups()
+                # the selection is either an array or an arguments list
+                # pass it on and then stop looping
+                # self.extract(**match.groupdict())
+                break
+
+            # expand the selection's scope
+            self.view.run_command('expand_selection', {'to': 'brackets'})
+            # get the contents of the new selection
+            new_selection = self.view.substr(self.view.sel()[0])
+
+            if new_selection == selection:
+                # if the new selection is the same as the old one,
+                # we've selected the whole text buffer and need to stop looping
+                break
+
+            # set the selection to the selection with increased scope and loop
+            selection = new_selection
+
+
+    def extract(self, array, args):
+        print array, args
+
+        if array:
+            # TODO parse items in array and generate list
+            # TODO start searching to right of region for arguments list
+            # TODO parse the arguments list and create mapping with array list
+            # show quick response panel with added items above
+            self.window.show_quick_panel(self.items,
+                                         self.handle_path_panel_response,
+                                         sublime.MONOSPACE_FONT, 0)
+        elif args:
+            # TODO parse items in arguments
+            # show quick response panel with added items above
+            self.window.show_quick_panel(self.name_items,
+                                         self.handle_name_panel_response,
+                                         sublime.MONOSPACE_FONT, 0)
+            pass
+
+
+    def handle_path_panel_response(self, index):
         if index is -1:
             return
         if index is 0:
@@ -83,6 +145,19 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
         module_id = self.module_collection.ids[index - 1]
         (module_varname, ext) = os.path.splitext(os.path.basename(module_id))
         self.handle_id_or_resource_input(module_id, module_varname)
+
+
+    def handle_name_panel_response(self, index):
+        if index is -1:
+            return
+        if index is 0:
+            self.window.show_input_panel("Module's variable name:", '',
+                                         self.handle_id_or_resource_input,
+                                         None, None)
+            return
+        # module_id = self.module_collection.ids[index - 1]
+        # (module_varname, ext) = os.path.splitext(os.path.basename(module_id))
+        # self.handle_id_or_resource_input(module_id, module_varname)
         # search = 'poo'
         # edit = self.view.begin_edit()
         # self.window.run_command('show_overlay', {'overlay': 'goto',
@@ -101,7 +176,7 @@ class AddRequirejsModuleDependencyCommand(sublime_plugin.WindowCommand):
 
     def handle_varname_input(self, input):
         print 'varname: ', input
-        self.create_define_enclosure()
+        # self.create_define_enclosure()
 
 
     # def create_define_enclosure(self):
